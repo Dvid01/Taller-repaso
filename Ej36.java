612775a55de1050d12f27191bb91a5cdcab5731a
Dvid01 <49034587+Dvid01@users.noreply.github.com>
2019-04-04 17:21:05 -0500

    36

---
+++ Ej36.java
@@ -0,0 +1,14 @@
+
+public class Ej36 {
+    public static void main(String[] args) {
+        System.out.println("Numeros pares del 1 al 25");
+        int i=0;
+        do{
+            i++;
+            if(i%2==0){
+                System.out.println(i);
+            }
+                
+        }while(i<=24);
+    }
+}
