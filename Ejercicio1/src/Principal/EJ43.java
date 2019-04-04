0be8b22d747624e151893253ad20b9a4285eeffa
Dvid01 <49034587+Dvid01@users.noreply.github.com>
2019-04-04 17:01:15 -0500

    ejercicio 34

---
+++ Ejercicio1/src/Principal/EJ43.java
@@ -0,0 +1,14 @@
+public class EJ43 {
+
+    public static void main(String[]args){
+        int i=1;
+        System.out.println("Nmeros del 1 al 10");
+        do{
+           
+              System.out.println(i);
+             i++;
+        }while(i<=10);
+            
+    }        
+}
+
