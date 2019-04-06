5bedfda4447ba04ed967672af56caac9ec101c3c
Dvid01 <49034587+Dvid01@users.noreply.github.com>
2019-04-05 21:32:00 -0500

    Add files via upload

---
+++ Ejercicio1/src/Principal/Ej44.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg38;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author luisdavid
+ */
+public class Ej44 {
+      public static void main(String[] args) {
+        long factorial=1;
+        int num;
+        Scanner numero = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        num = numero.nextInt();
+        for (int i = num; i > 0; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("El factorial de " + num + " es: " + factorial);
+    }
+}
+
