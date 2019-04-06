f31bf727299592a60a1a356a2da6bd7de0e37965
Dvid01 <49034587+Dvid01@users.noreply.github.com>
2019-04-05 20:28:21 -0500

    Add files via upload

---
+++ Ejercicio1/src/Principal/EJ38.java
@@ -0,0 +1,12 @@
+package pkg38;
+
+public class EJ38 {
+   public static void main (String[]args){
+       int suma=0;
+       for(int i=1;i<=10;i++){
+           suma=suma+i;
+           System.out.println(suma);
+       }
+   }
+           
+}
